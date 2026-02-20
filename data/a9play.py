d6846ecd554e11f5de03c173665e8b8dbb2f0af3
hamikya666 <hamikya66@gmail.com>
2026-02-21 04:41:25 +0800

    Create a9play.py

---
+++ data/a9play.py
@@ -0,0 +1,27 @@
+# data/cm8.py
+
+PLATFORMS = {
+    "VPOWER": [
+        "DolphinReef",
+        "Mahjong Ways 2",
+        "Triple Supreme Olympic",
+        "RAVE FEVER PARTY",
+        "Cash Machine"
+    ],
+
+    "BAOZHUZHAOFU": [
+        "Starlight Princess",
+        "PIRATE BINGO",
+        "GOLDEN CENTURY",
+        "BAOZHUZHAOCAI",
+        "Fortune Bowls"
+    ],
+
+    "HACKSAW": [
+        "Stick'Em",
+        "OmNom",
+        "Miami Multiplier",
+        "Cubes",
+        "Cash Compass"
+    ]
+}
