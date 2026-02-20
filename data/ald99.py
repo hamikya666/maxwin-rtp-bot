df5c95e5392d3a8896901c11bf96738fecc3cfac
hamikya666 <hamikya66@gmail.com>
2026-02-21 04:47:22 +0800

    Create ald99.py

---
+++ data/ald99.py
@@ -0,0 +1,15 @@
+# data/ald99.py
+
+PLATFORMS = {
+    "PP": [
+        "SuperAce Plus",
+        "Wisdom Athena 1000",
+        "Sweet Bonanza 1000"
+    ],
+
+    "PG": [
+        "Fortune Panda",
+        "Fortune Tiger",
+        "Starlight Princess 1000"
+    ]
+}
