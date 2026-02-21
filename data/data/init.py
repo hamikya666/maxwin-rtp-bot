0138789e3ab99a454450c8fdff1f07e4c091b240
hamikya666 <hamikya66@gmail.com>
2026-02-21 16:10:44 +0800

    Update and rename data/__init__.py to data/data/init.py

--- data/data/init.py
+++ data/data/init.py
@@ -1,10 +1,8 @@
-# data/__init__.py
 from . import cm8, a9play, ald99, u9play
 
-# 所有商家及对应平台
 MERCHANTS = {
     "CM8": cm8.PLATFORMS,
     "A9PLAY": a9play.PLATFORMS,
     "ALD99": ald99.PLATFORMS,
-    "U9PLAY": u9play.PLATFORMS
+    "U9PLAY": u9play.PLATFORMS,
 }
