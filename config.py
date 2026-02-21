8c62496b1c8562281195a472e227c55fa88a543c
hamikya666 <hamikya66@gmail.com>
2026-02-22 06:33:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "8194393436:AAF-fVYwsGflkXyHU7nOg7vmOJV40fRiDIM"
-ADMIN_IDS = [5473935017]  # 改成你的telegram ID
+ADMIN_ID = 5473935017
 
 VIDEO_FILE_ID = "BAACAgUAAxkBAAJ682mYXMwrOUSatmP8ROjQJcx6vtw9AAI1HAACd5HBVPGdMpbcTHcZOgQ"
 
@@ -8,4 +8,11 @@ MERCHANT_LINKS = {
     "A9PLAY": "http://a9play5.com/R=F7464F",
     "ALD99": "https://bit.ly/ALDMaxWin",
     "U9PLAY": "https://u9play99.com/R=C8BAAC"
+}
+
+MERCHANT_MODULES = {
+    "CM8": "data.CM8",
+    "A9PLAY": "data.A9PLAY",
+    "ALD99": "data.ALD99",
+    "U9PLAY": "data.U9PLAY"
 }
