c2411b916f894f10634b8fddd6a4638029729576
hamikya666 <hamikya66@gmail.com>
2026-02-21 14:31:02 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,12 +1,7 @@
 # data/__init__.py
+from . import cm8, a9play, ald99, u9play
 
-from . import cm8
-from . import a9play
-from . import ald99
-from . import u9play
-
-# 也可以统一做个字典导出方便bot引用
-ALL_MERCHANTS = {
+MERCHANTS = {
     "CM8": cm8.GAMES,
     "A9PLAY": a9play.GAMES,
     "ALD99": ald99.GAMES,
