4e2b68ff30947f6ec5ad1acf919774de0de3c268
hamikya666 <hamikya66@gmail.com>
2026-02-21 14:37:51 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,9 +1,10 @@
 # data/__init__.py
 from . import cm8, a9play, ald99, u9play
 
+# 所有商家及对应平台
 MERCHANTS = {
-    "CM8": cm8.GAMES,
-    "A9PLAY": a9play.GAMES,
-    "ALD99": ald99.GAMES,
-    "U9PLAY": u9play.GAMES
+    "CM8": cm8.PLATFORMS,
+    "A9PLAY": a9play.PLATFORMS,
+    "ALD99": ald99.PLATFORMS,
+    "U9PLAY": u9play.PLATFORMS
 }
