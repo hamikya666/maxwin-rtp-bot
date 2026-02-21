5dde08bbc0bf6cd102dbb07ce17abf523724ae7c
hamikya666 <hamikya66@gmail.com>
2026-02-22 06:38:15 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,29 @@
-from telegram.ext import ApplicationBuilder
-from config import TOKEN
-from handlers import start, register, scan, admin, wallet, referral
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from config import TOKEN, VIDEO_FILE_ID
+from handlers import register, scan, admin, wallet, referral
+
+async def start(update, context):
+    keyboard = [
+        [InlineKeyboardButton("🎮 Scan RTP", callback_data="scan_menu")],
+        [InlineKeyboardButton("📝 Daftar", callback_data="register")],
+        [InlineKeyboardButton("💰 DOMPET Boss", callback_data="wallet")],
+        [InlineKeyboardButton("🔗 SHARE AND EARN", callback_data="ref")]
+    ]
+
+    await update.message.reply_video(
+        VIDEO_FILE_ID,
+        caption="🔥Selamat datang ke MAXWIN AI RTP\n🤖AI yang scan RTP tertinggi dalam slot2\n📊 Tekan game menu di bawah untuk mula",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-start.setup(app)
-register.setup(app)
-scan.setup(app)
-admin.setup(app)
-wallet.setup(app)
-referral.setup(app)
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(register.show_register, pattern="register"))
+app.add_handler(CallbackQueryHandler(scan.scan_menu, pattern="scan_menu"))
+app.add_handler(CallbackQueryHandler(wallet.wallet_handler, pattern="wallet"))
+app.add_handler(CallbackQueryHandler(referral.referral_handler, pattern="ref"))
 
-print("Bot Running...")
+print("BOT RUNNING...")
 app.run_polling()
