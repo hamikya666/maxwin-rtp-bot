4040bd9314b9727c23208fb83934fa4968a6a93d
hamikya666 <hamikya66@gmail.com>
2026-02-21 20:16:56 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+VIDEO_FILE_ID = "BAACAgUAAxkBAAJ682mYXMwrOUSatmP8ROjQJcx6vtw9AAI1HAACd5HBVPGdMpbcTHcZOgQ"
+
+MERCHANT_LINKS = {
+    "CM8": "https://bit.ly/MaxWinCM8",
+    "A9PLAY": "http://a9play5.com/R=F7464F",
+    "ALD99": "https://bit.ly/ALDMaxWin",
+    "U9PLAY": "https://u9play99.com/R=C8BAAC",
+}
+
+SCAN_VALIDITY_MINUTES = 15
+REFERRAL_REWARD = 1  # RM1 per invite
+MIN_WITHDRAWAL = 50
